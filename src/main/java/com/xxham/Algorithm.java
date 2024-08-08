d981ce28778940b512da93edd2014c4e1627594c
KeepFocusL <1281438594@qq.com>
2024-08-08 11:36:28 +0800

    Init

---
+++ src/main/java/com/xxham/Algorithm.java
@@ -0,0 +1,11 @@
+package com.xxham;
+
+/**
+ * Abstraction of specific algorithms.
+ */
+public abstract class Algorithm {
+    /**
+     * Storing the specific logic of the algorithm
+     */
+    public abstract void execute();
+}
